407b0dcb91162883df783b39369d1ad8cbea67a0
Amjad Alhouseini <amjadm2008@gmail.com>
2017-02-04 18:55:05 +0800

    Add Main File to the project

---
+++ Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ABS(x) ((x)<0 ? -(x) : (x))
+#define REP(i,n) for(int i=0, _e(n); i<_e; i++)
+#define SET(t,v) memset((t), (v), sizeof(t))
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize( unique( ALL(c) ) - (c).begin() )
+
+#define inf_int (1<<31)-1
+#define inf_long (1<<63)-1
+#define sz size()
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+
+typedef long long LL;
+typedef long double LD;
+typedef pair<int,int> pii;
+
+
+#ifdef DEBUG
+#define dbg(x) x
+#define dbgp(x) cerr << x ;
+#else
+#define dbg(x) //x
+#define dbgp(x) //cerr << x << endl;
+#endif
+
+
+int main(){
+    
+    
+    return 0;
+}
